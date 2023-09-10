b37707b0c23456c9c261094525f70d4876a31e4b
Kanish <111266024+Kanish-Sk@users.noreply.github.com>
2023-09-10 14:20:32 +0530

    cors setting change

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require('cors');
 const fs = require('fs');
 
 const placesRouter = require("./routes/places-routes");
@@ -16,7 +15,16 @@ app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
 
 app.use("/api/places", placesRouter);
 
